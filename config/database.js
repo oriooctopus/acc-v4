e82a707deca81f219f7ddda7f7c4d7257a494b8c
Oliver Ullman <oliverullman@gmail.com>
2021-12-23 21:39:11 -0300

    debugging

--- config/database.js
+++ config/database.js
@@ -1,13 +1,14 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'acc-v4'),
-      user: env('DATABASE_USERNAME', 'username'),
-      password: env('DATABASE_PASSWORD', 'password'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      // port: env.int('DATABASE_PORT', 5432),
+      port: env.int("DATABASE_PORT", 1111),
+      database: env("DATABASE_NAME", "acc-v4"),
+      user: env("DATABASE_USERNAME", "username"),
+      password: env("DATABASE_PASSWORD", "password"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
   },
 });
