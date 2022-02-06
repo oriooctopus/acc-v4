9819671142b8d0cf1c331d75525011f30aa37791
Oliver Ullman <oliverullman@gmail.com>
2022-02-06 00:18:31 -0300

    Add getNextLessonSlug query

--- src/api/lesson/content-types/lesson/lifecycles.js
+++ src/api/lesson/content-types/lesson/lifecycles.js
@@ -1,10 +1,5 @@
 const enforceSortingOrder = require("../../../../utils/enforceSortingOrder");
 
-const sublessonLessonLinkFields = ["lesson_id", "sublesson_id"];
-// had trouble with serializing these so removing them for now
-const temporaryIgnore = ["created_at", "updated_at", "published_at"];
-const fieldsToIgnore = ["id", ...sublessonLessonLinkFields, ...temporaryIgnore];
-
 module.exports = {
   async beforeUpdate(event) {
     const newSublessonChallengeIds = await enforceSortingOrder({
@@ -12,14 +7,6 @@ module.exports = {
       orderedEntityIds: event.params.data.sublessons,
     });
 
-    // updates the intended order to use the newly created stuff~
-    // setSublessonChallengeIds(newSublessonChallengeIds);
     event.params.data.sublessons = newSublessonChallengeIds;
-    // SELECT * FROM sublessons_lesson_links WHERE lesson_id = ${lessonId}
-
-    // const sublessonChallenges = await strapi.db.connection.raw(`
-    //   SELECT * sublessons WHERE id =
-    // `);
-    // event.params.data.name = event.params.data.name + "7";
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = {
   /**
@@ -7,7 +7,72 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {
+    const extensionService = strapi.plugin("graphql").service("extension");
+
+    const extension = ({ nexus }) => ({
+      // Nexus
+      types: [
+        nexus.queryType({
+          definition(t) {
+            t.field("nextLessonSlug", {
+              type: "String",
+              args: {
+                currentLessonId: nexus.intArg(),
+              },
+            });
+          },
+        }),
+      ],
+      // GraphQL SDL
+      typeDefs: `
+          type Article {
+              name: String
+          }
+      `,
+      resolvers: {
+        Query: {
+          nextLessonSlug: {
+            async resolve(_context, { currentLessonId }) {
+              console.log("before");
+              const result = await strapi.db
+                .query("api::module.module")
+                .findOne({
+                  where: {
+                    id: 1,
+                  },
+                  populate: {
+                    moduleLessons: {
+                      populate: ["lesson"],
+                    },
+                  },
+                });
+              const { moduleLessons: lessons } = result;
+              const currentLessonIndex = lessons.findIndex(
+                ({ lesson: { id } }) => id === currentLessonId
+              );
+
+              if (currentLessonIndex === -1) {
+                throw new Error(`Lesson of id ${currentLessonId} not found`);
+              }
+
+              if (currentLessonIndex === lessons.length - 1) {
+                return null;
+              }
+
+              return lessons[currentLessonIndex + 1].lesson.slug;
+            },
+          },
+        },
+      },
+      resolversConfig: {
+        "Query.nextLessonSlug": {
+          auth: false,
+        },
+      },
+    });
+    extensionService.use(extension);
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
