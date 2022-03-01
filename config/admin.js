65dd9da28d121837a12762054ddb5dcb55cc271a
bjornrl <bjornrleira@gmail.com>
2022-03-01 15:38:55 -0300

    Strapi config sync up again

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '9f57058c01429d625346594277bcfcf4'),
+    secret: env("ADMIN_JWT_SECRET", "9f57058c01429d625346594277bcfcf4"),
   },
+  watchIgnoreFiles: ["**/config/sync/**"],
 });
