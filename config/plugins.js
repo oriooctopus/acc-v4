38626d3a2085008b6fd2b319e8b5958f3ff1b659
bjornrl <87340318+bjornrl@users.noreply.github.com>
2022-03-18 13:40:02 +0100

    Strapi config sync v2 (#6)

--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,12 @@
 module.exports = {
   graphql: {
     config: {
+      syncDir: "config/sync",
+      minify: false,
+      importOnBootstrap: false,
+      customTypes: [],
+      excludedTypes: [],
+      excludedConfig: [],
       defaultLimit: 100,
     },
   },
