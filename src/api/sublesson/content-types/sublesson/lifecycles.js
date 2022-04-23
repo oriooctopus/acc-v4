4c47bb3113d0d5bea530ec3c88acf9038c2f2cae
Oliver Ullman <oliverullman@gmail.com>
2022-04-23 22:24:00 +0200

    Add internalLabel for sublessons

---
+++ src/api/sublesson/content-types/sublesson/lifecycles.js
@@ -0,0 +1,35 @@
+const pMap = require("p-map");
+
+/**
+ * TODO:
+ * This is pretty inefficient. Look into either simple or
+ * more complex optimizations
+ */
+const addInternalLabel = async (sublesson) => {
+  const populatedSublesson = await strapi.db
+    .query("api::sublesson.sublesson")
+    .findOne({
+      where: {
+        id: sublesson.id,
+      },
+      populate: ["lesson"],
+    });
+
+  sublesson.internalLabel = populatedSublesson.lesson
+    ? `${populatedSublesson.lesson.name} - ${sublesson.name}`
+    : `Unassigned - ${sublesson.name}`;
+};
+
+module.exports = {
+  async afterFindOne(event) {
+    console.log("hey does this happen &*&*&>>????");
+    addInternalLabel(event.data);
+  },
+
+  async afterFindMany(event) {
+    console.log("hey does this happen &*&*&!!");
+    // await pMap(event.results, addInternalLabel, { concurrency: 20 });
+    await pMap(event.result, addInternalLabel, { concurrency: 20 });
+    // event.result.forEach(addInternalLabel);
+  },
+};
